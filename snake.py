b4c94fe5d7922de99456bc5b93a6ee50c8a179c4
O-T-O-Z <60617932+O-T-O-Z@users.noreply.github.com>
2020-08-13 17:52:35 +0200

    While loop added

--- snake.py
+++ snake.py
@@ -1,7 +1,13 @@
 import pygame
+
 pygame.init()
 dis=pygame.display.set_mode((400,300))
 pygame.display.update()
+pygame.display.set_caption('Snake game by Me')
+game_over = False
+while not game_over:
+	for event in pygame.event.get():
+		print(event)
 pygame.quit()
 quit()
 
