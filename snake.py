9df8cc04f474751978c045e1cf5be3099b028031
O-T-O-Z <60617932+O-T-O-Z@users.noreply.github.com>
2020-08-14 12:20:14 +0200

    Boundaries are now game over

--- snake.py
+++ snake.py
@@ -1,10 +1,14 @@
 import pygame
+import time
 
 pygame.init()
-dis = pygame.display.set_mode((800, 600))
+width = 800
+height = 600
+speed = 30
+dis = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Snake game by Me')
 
-#colors
+# Colors
 blue = (0, 0, 255)
 red = (255, 0, 255)
 black = (0, 0, 0)
@@ -12,13 +16,21 @@ white = (255, 255, 255)
 
 game_over = False
 
-x1 = 300
-y1 = 300
+x1 = width/2
+y1 = height/2
 
 x1_change = 0
 y1_change = 0
 
 clock = pygame.time.Clock()
+
+font_style = pygame.font.SysFont(None, 50)
+
+
+def message(msg, color):
+	mes = font_style.render(msg, True, color)
+	dis.blit(mes, [width/2, height/2])
+
 
 while not game_over:
 	for event in pygame.event.get():
@@ -37,12 +49,16 @@ while not game_over:
 			elif event.key == pygame.K_DOWN:
 				x1_change = 0
 				y1_change = 10
+	if x1 <= 0 or x1 >= width or y1 <= 0 or y1 >= 0:
+		game_over = True
 	x1 += x1_change
 	y1 += y1_change
 
 	pygame.draw.rect(dis, blue, [x1, y1, 10, 10])
 	pygame.display.update()
-	clock.tick(30)
+	clock.tick(speed)
+
+message("Game Over", white)
 pygame.quit()
 quit()
 
