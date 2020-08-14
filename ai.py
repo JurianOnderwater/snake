a6fb7d7e0e760988ef81240586a08501fbaeacc1
O-T-O-Z <60617932+O-T-O-Z@users.noreply.github.com>
2020-08-14 13:47:57 +0200

    Keyboard input test

--- ai.py
+++ ai.py
@@ -2,5 +2,11 @@ import pygame
 import time
 import random
 from snake import *
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
 
 gameLoop()
+time.sleep(5)
+keyboard.press(Key.up)
+keyboard.release(Key.up)
