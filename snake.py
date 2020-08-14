fceb08e3f99fdfdaafa4d1f493efcc0e0bbe5c17
O-T-O-Z <60617932+O-T-O-Z@users.noreply.github.com>
2020-08-14 12:03:37 +0200

    Key presses

--- snake.py
+++ snake.py
@@ -10,15 +10,40 @@ red = (255, 0, 255)
 black = (0, 0, 0)
 white = (255, 255, 255)
 
-
-
 game_over = False
+
+x1 = 300
+y1 = 300
+
+x1_change = 0
+y1_change = 0
+
+clock = pygame.time.Clock()
+
 while not game_over:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			game_over = True
-	pygame.draw.rect(dis, blue, [200, 150, 10, 10])
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K.LEFT:
+				x1_change = -10
+				y1_change = 0
+			elif event.key == pygame.K.RIGHT:
+				x1_change = 10
+				y1_change = 0
+			elif event.key == pygame.K.UP:
+				x1_change = 0
+				y1_change = -10
+			elif event.key == pygame.K.DOWN:
+				x1_change = 0
+				y1_change = 10
+	x1 += x1_change
+	y1 += y1_change
+	dis.fill(white)
+
+	pygame.draw.rect(dis, black, [200, 150, 10, 10])
 	pygame.display.update()
+	clock.tick(30)
 pygame.quit()
 quit()
 
